560e6b74d457aae165d41f9705a8054508794ca1
Henry Larkin <hfglarkin@gmail.com>
2021-12-05 14:33:34 +0000

    day 2 part 2 correct

--- 2021/2.rb
+++ 2021/2.rb
@@ -17,3 +17,23 @@ lines.each do |line|
 end
 
 puts "part 1: #{forward * depth}"
+
+forward = 0
+depth = 0
+aim = 0
+
+lines.each do |line|
+  command, value = line.split(" ")
+
+  case command
+  when "forward"
+    forward += value.to_i
+    depth += aim * value.to_i
+  when "down"
+    aim += value.to_i
+  when "up"
+    aim -= value.to_i
+  end
+end
+
+puts "part 2: #{forward * depth}"
